1ddeff3d3756eb08e5fde99eb0ec350de1acd270
antoniomontenegro83 <antoniomontenegro83@gmail.com>
2025-05-14 23:40:18 +0200

    Delete frontend directory

--- frontend/js/management-turni.js
+++
@@ -1,5 +0,0 @@
-/**
- * management-turni.js - Entry point per la pagina gestione turni
- */
-import './modules/turni/index.js';
-console.log("Pagina management-turni caricata");
--- frontend/js/users-privileges.js
+++
@@ -1,6 +0,0 @@
-// frontend/js/users-privileges.js
-/**
- * users-privileges.js - Entry point per la pagina gestione privilegi
- */
-import './modules/privilegi/index.js';
-console.log("Pagina gestione privilegi caricata");
