4b977d7096032ee3cd246a8d3f9908ffdb4df88e
antoniomontenegro83 <antoniomontenegro83@gmail.com>
2025-05-19 16:34:49 +0200

    Delete backend_gestione_ore directory

--- backend_gestione_ore/script/qualifiche/list.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../db.php';
-header("Content-Type: application/json");
-
-$stmt = $pdo->query("SELECT id, qualifica FROM qualifiche ORDER BY id");
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
