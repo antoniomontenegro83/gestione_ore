1562e49781a21a26c0a6ba876e40274951301f89
antoniomontenegro83 <antoniomontenegro83@gmail.com>
2025-05-22 11:45:22 +0200

    Delete backend_gestione_ore directory

--- backend_gestione_ore/report/Nuova cartella/report.php
+++
@@ -1,8 +0,0 @@
-<?php
-// File indice per la cartella report
-// Questo file serve principalmente a impedire la navigazione diretta della directory
-
-// Reindirizza alla pagina principale
-header('Location: ../index.php');
-exit;
-?>
