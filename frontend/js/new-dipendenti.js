e8b11fcd92d9d13e8af4b515f55dc1500132645b
antoniomontenegro83 <antoniomontenegro83@gmail.com>
2025-05-22 11:39:59 +0200

    Delete frontend directory

--- frontend/js/new-dipendenti.js
+++
@@ -1,5 +0,0 @@
-/**
- * new-dipendenti.js - Entry point per la pagina nuovo dipendente
- */
-import './modules/new-dipendente/index.js';
-console.log("Pagina new-dipendenti caricata");
--- frontend/js/time-entry.js
+++
@@ -1,5 +0,0 @@
-/**
- * time-entry.js - Entry point per la pagina inserimento turni
- */
-import './modules/time-entry/index.js';
-console.log("Pagina time-entry caricata");
